ff32b7a2beacf1bcb1bf4bf3c42ef6597a2cacb5
Najma Saeed <najmasy20@gmail.com>
2023-10-03 13:44:33 +0100

    added js and css grid

--- app.js
+++ app.js
@@ -1,30 +1,46 @@
 // DOM Nodes
-let productsContainer = document.querySelector(".container");
-let image1 = document.querySelector("section img:first-child");
-let image2 = document.querySelector("section img:nth-child(2)");
-let image3 = document.querySelector("section img:nth-child(3)");
+let image1 = document.getElementById("image1");
+let image2 = document.getElementById("image2");
+let image3 = document.getElementById("image3");
 
-// keep each product in an object
-function Products(name, src) {
+//make sure the user only has 25 clicks
+let userClicks = 0;
+let maxClicks = 25;
+
+// keep each product in an object in a constructor
+function Product(name) {
   this.name = name;
-  this.src = src;
+  this.src = `./images/${name}.jpg`;
   this.views = 0;
   this.clicks = 0;
 }
 
-// function to choose a random product
+// make the products
+const products = [
+  new Product("bag"),
+  new Product("banana"),
+  new Product("boots"),
+  new Product("breakfast"),
+  new Product("bubblegum"),
+  new Product("cthulhu"),
+  new Product("dog-duck"),
+  new Product("sweep"),
+  new Product("tauntaun"),
+];
+
+// function to choose a random index
 function getRandomIndex() {
-  return Math.floor(Math.random() * allProducts.length);
+  return Math.floor(Math.random() * products.length);
 }
 
-// function to render 2 random products
-function renderGoats() {
+// function to render 3 random products on the page (using the 3 img tags)
+function renderProducts() {
   // get 2 random indexes from our products array
   let product1Index = getRandomIndex();
   let product2Index = getRandomIndex();
   let product3Index = getRandomIndex();
 
-  // prevent the two images being the same product
+  // prevent the three images being the same product
   while (
     product1Index === product2Index ||
     product2Index === product3Index ||
@@ -35,63 +51,66 @@ function renderGoats() {
   }
 
   // change the src of our 3 images
-  image1.src = allProducts[product1Index].src;
-  image2.src = allProducts[product2Index].src;
-  image3.src = allProducts[product3Index].src;
-  image1.alt = allProducts[product1Index].name;
-  image2.alt = allProducts[product2Index].name;
-  image3.alt = allProducts[product3Index].name;
+  image1.src = products[product1Index].src;
+  image2.src = products[product2Index].src;
+  image3.src = products[product3Index].src;
+  image1.alt = products[product1Index].name;
+  image2.alt = products[product2Index].name;
+  image3.alt = products[product3Index].name;
 
   // increase the product views
-  allProducts[product1Index].views++;
-  allProducts[product2Index].views++;
-  allProducts[product3Index].views++;
+  products[product1Index].views++;
+  products[product2Index].views++;
+  products[product3Index].views++;
 }
 
-// handle the product being clicked
-function handleProductClick(event) {
+// handle what happens when the img is being clicked
+function handleImgClick(event) {
+  //render 3 more images
+  // check if the clicks has run out
+  if (userClicks >= maxClicks) {
+    alert("you have run out of votes");
+    return;
+  }
+  //increase number of time the user has clicked
+  userClicks++;
+
   // get the name of the product we just clicked
   let clickedProduct = event.target.alt;
-
-  // check if the click is on an image
-  if (event.target === productsContainer) {
-    alert("Please click on an image");
-  } else {
-    // render more products
-    renderProducts();
-  }
-
-  // increase the clicks of the product
-  // loop through allProducts
-  for (let i = 0; i < allProducts.length; i++) {
+  //increase the clicks of the product
+  for (let i = 0; i < products.length; i++) {
     // check if the name of the product in the array, matches the alt tag of our image
-    if (clickedProduct === allProducts[i].name) {
+    if (clickedProduct === products[i].name) {
       // increase the number of clicks
-      allProducts[i].clicks++;
+      products[i].clicks++;
       // stop the for loop because we found the product
       break;
     }
   }
+  renderProducts();
 }
 
-// make the products
-const allProducts = [
-  new Product("Bag", "./images/bag.jpg"),
-  new Product("Banana", "./images/banana.jpg"),
-  new Product("Boots", "./images/boots.jpg"),
-  new Product("Breakfast", "./images/brakfast.jpg"),
-  new Product("Bubblegum", "./images/bubblegum.jpg"),
-  new Product("Cthulhu", "./images/cthulhu.jpg"),
-  new Product("Dog Duck", "./images/dog-duck.jpg"),
-  new Product("Sweep", "./images/sweep.png"),
-  new Product("Tauntaun", "./images/tauntaun.jpg"),
-];
+image1.addEventListener("click", handleImgClick);
+image2.addEventListener("click", handleImgClick);
+image3.addEventListener("click", handleImgClick);
 
-// render the results
-// when the user clicks the view results button
-// render a ul full of li's that tell the user how many times each product has been clicked
-
-// add the event listener to the product
-productsContainer.addEventListener("click", handleProductClick);
+//show results
+function showResults() {
+  // render the results
+  // when the user clicks the view results button
+  // render a ul full of li's that tell the user how many times each product has been clicked
+  const results = document.getElementById("results");
+  //put lis into a ul
+  //loop products
+  for (let i = 0; i < products.length; i++) {
+    const li = document.createElement("li");
+    const product = products[i];
+    li.textContent = `${product.name} was viewed ${product.views} times, and clicked ${product.clicks} times`;
+    results.appendChild(li);
+  }
+}
+// make button view results
+const viewResults = document.getElementById("view-results");
+viewResults.addEventListener("click", showResults);
 
 renderProducts();
