28675fa716388aacf8848b231970b56740a4708d
Najma Saeed <najmasy20@gmail.com>
2023-10-04 15:44:53 +0100

    added chart js

--- app.js
+++ app.js
@@ -5,73 +5,98 @@ let image3 = document.getElementById("image3");
 
 //make sure the user only has 25 clicks
 let userClicks = 0;
-let maxClicks = 25;
+const maxClicks = 25;
+
+const products = [];
 
 // keep each product in an object in a constructor
-function Product(name) {
+function Product(name, views, clicks) {
   this.name = name;
   this.src = `./images/${name}.jpg`;
   this.views = 0;
   this.clicks = 0;
+
+  //take new object that is created, and put it into the array
+  products.push(this)
 }
 
 // make the products
-const products = [
-  new Product("bag"),
-  new Product("banana"),
-  new Product("bathroom"),
-  new Product("boots"),
-  new Product("breakfast"),
-  new Product("bubblegum"),
-  new Product("chair"),
-  new Product("cthulhu"),
-  new Product("dog-duck"),
-  new Product("dragon"),
-  new Product("pen"),
-  new Product("pet-sweep"),
-  new Product("scissors"),
-  new Product("shark"),
-  new Product("sweep"),
-  new Product("tauntaun"),
-  new Product("unicorn"),
-  new Product("water-can"),
-  new Product("wine-glass"),
-];
+//if there is nothing in local storage, instantiate/create default/zero clicks products
+//BUT if there is something in local storage, get that, and turn into my products
+
+if (localStorage.getItem('products') === null) {
+  new Product("bag", 0, 0);
+  new Product("banana", 0, 0);
+  new Product("bathroom", 0, 0);
+  new Product("boots", 0, 0);
+  new Product("breakfast", 0, 0);
+  new Product("bubblegum", 0, 0);
+  new Product("chair", 0, 0);
+  new Product("cthulhu", 0, 0);
+  new Product("dog-duck", 0, 0);
+  new Product("dragon", 0, 0);
+  new Product("pen", 0, 0);
+  new Product("pet-sweep", 0, 0);
+  new Product("scissors", 0, 0);
+  new Product("shark", 0, 0);
+  new Product("sweep", 0, 0);
+  new Product("tauntaun", 0, 0);
+  new Product("unicorn", 0, 0);
+  new Product("water-can", 0, 0);
+  new Product("wine-glass", 0, 0);
+} else {
+  const productsLS = JSON.parse(localStorage.getItem("products"))
+  //LS is local storage
+  //for each item in the productsLS array, make a new product
+  for (let i = 0; i < productsLS.length; i++){
+    //create a new product for each item in the array (ad the Product function automatically adds it to the array)
+    //instantiate - get the first item name/views/clicks from local storage then 2nd etc...
+    new Product(productsLS[i].name, productsLS[i].views, productsLS[i].clicks);
+
+  }
+}
+
+
 
 // function to choose a random index
 function getRandomIndex() {
-  return Math.floor(Math.random() * products.length);
+  return Math.floor(Math.random() * products.length)
 }
 
 // function to render 3 random products on the page (using the 3 img tags)
 function renderProducts() {
   // get 2 random indexes from our products array
-  let product1Index = getRandomIndex();
-  let product2Index = getRandomIndex();
-  let product3Index = getRandomIndex();
+  let chosenProductIndex = [],
 
-  // prevent the three images being the same product
-  while (
-    product1Index === product2Index ||
-    product2Index === product3Index ||
-    product1Index === product3Index
-  ) {
-    product2Index = getRandomIndex();
-    product3Index = getRandomIndex();
+  // prevent the three images being the same product and in the next attempt after each click
+  function getUniqueRandomIndex() {
+    let randomIndex = getRandomIndex(),
+
+    while (chosenProductIndex.includes(randomIndex)) {
+      randomIndex = getUniqueRandomIndex();
+    }
+    return randomIndex;
   }
+  //next set of products are different from the previous set
+  for (let i = 0; i < 3; i++) {
+    chosenProductIndex[i] = getUniqueRandomIndex();
+  }
+  console.log(getUniqueRandomIndex)
 
+
+  
   // change the src of our 3 images
-  image1.src = products[product1Index].src;
-  image2.src = products[product2Index].src;
-  image3.src = products[product3Index].src;
-  image1.alt = products[product1Index].name;
-  image2.alt = products[product2Index].name;
-  image3.alt = products[product3Index].name;
+  image1.src = products[chosenProductIndex[0]].src,
+  image2.src = products[chosenProductIndex[1]].src,
+  image3.src = products[chosenProductIndex[2]].src,
+  image1.alt = products[chosenProductIndex[0]].name,
+  image2.alt = products[chosenProductIndex[1]].name,
+  image3.alt = products[chosenProductIndex[2]].name,
 
   // increase the product views
-  products[product1Index].views++;
-  products[product2Index].views++;
-  products[product3Index].views++;
+  products[chosenProductIndex[0]].views++;
+  products[chosenProductIndex[1]].views++;
+  products[chosenProductIndex[2]].views++;
 }
 
 // handle what happens when the img is being clicked
@@ -80,6 +105,9 @@ function handleImgClick(event) {
   // check if the clicks has run out
   if (userClicks >= maxClicks) {
     alert("you have run out of votes");
+    renderChart();
+    //take our array after we have updated the clicks and views, and add to localStorage
+    localStorage.setItem("products", JSON.stringify(products));
     return;
   }
   //increase number of time the user has clicked
@@ -97,15 +125,15 @@ function handleImgClick(event) {
       break;
     }
   }
-  renderProducts();
+  
 }
 
-image1.addEventListener("click", handleImgClick);
-image2.addEventListener("click", handleImgClick);
-image3.addEventListener("click", handleImgClick);
+image1.addEventListener("click", handleImgClick),
+image2.addEventListener("click", handleImgClick),
+image3.addEventListener("click", handleImgClick),
 
 // make button view results
-const viewResults = document.getElementById("view-results");
+const viewResults = document.getElementById("view-results")
 viewResults.addEventListener("click", showResults);
 
 renderProducts();
@@ -128,7 +156,7 @@ function showResults() {
 }
 
 function renderChart() {
-  const ctx = document.getElementById("myChart");
+  const ctx = document.getElementById("myChart"),
 
   const labels = [];
   const views = [];
@@ -160,3 +188,5 @@ function renderChart() {
     },
   });
 }
+
+renderProducts();
